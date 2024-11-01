75c2380aab92ed4d6f4ed293a6aa5dd46c2a6407
Jacob McCollum <jacob.r.mccollum@gmail.com>
2024-11-01 10:13:42 -0400

    Sketch out some ultra basic navigation & editing. This is in no way final or prescriptive, but wanted to have some very rudimentary idea of what information is required to place & draw shapes.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,37 @@
 <script setup lang="ts">
-import { ref, computed, onMounted, onUnmounted } from 'vue';
+import { ref, computed, onMounted, onUnmounted, reactive } from 'vue';
+import { nanoid } from 'nanoid';
+
+interface IShapeBase {
+  id: string;
+  boundingBox: {
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+  };
+}
+
+interface IShapeRect extends IShapeBase {
+  kind: 'rect';
+}
+
+interface IShapeCircle extends IShapeBase {
+  kind: 'circle';
+  center: {
+    x: number;
+    y: number;
+  },
+  radius: number;
+}
+
+type IShape = IShapeRect | IShapeCircle;
 
 const offsetX = ref(0.0);
 const offsetY = ref(0.0);
+
+const mouseScreenX = ref(0.0);
+const mouseScreenY = ref(0.0);
 
 const windowWidth = ref(window.innerWidth);
 const windowHeight = ref(window.innerHeight);
@@ -11,33 +40,67 @@ const viewBox = computed(() => {
   return `${offsetX.value} ${offsetY.value} ${windowWidth.value} ${windowHeight.value}`;
 });
 
+const cursorStyle = computed(() => {
+  return `top: ${mouseScreenY.value}px; left: ${mouseScreenX.value}px;`;
+});
+
+const shapes = reactive<IShape[]>([]);
+
 const onWindowResized = () => {
   windowWidth.value = window.innerWidth;
   windowHeight.value = window.innerHeight;
 };
 
+const screenToBoardCoordinates = (pos: { x: number, y: number }) => {
+  return { x: pos.x + offsetX.value, y: pos.y + offsetY.value };
+};
 
-let isDragging = false;
-let lastDragX = 0.0;
-let lastDragY = 0.0;
+const boardToScreenCoordinates = (pos: { x: number, y: number }) => {
+  return { x: pos.x - offsetX.value, y: pos.y - offsetY.value };
+};
+
+const isDragging = ref(false);
 
 const onMouseDown = (event: MouseEvent) => {
-  isDragging = true;
-  lastDragX = event.clientX;
-  lastDragY = event.clientY;
+  switch (event.button) {
+    case 0: // left click
+    {
+      const center = screenToBoardCoordinates({ x: event.clientX, y: event.clientY });
+      shapes.push({
+        id: nanoid(),
+        kind: 'circle',
+        boundingBox: {
+          x: center.x - 20,
+          y: center.y - 20,
+          width: 40,
+          height: 40,
+        },
+        center,
+        radius: 20,
+      });
+      break;
+    }
+    case 1: // middle/wheel click
+      isDragging.value = true;
+      break;
+    case 2: // right click
+      break;
+    default:
+      break;
+  }
 };
 
 const onMouseUp = () => {
-  isDragging = false;
+  isDragging.value = false;
 };
 
 const onMouseMove = (event: MouseEvent) => {
-  if (isDragging) {
-    offsetX.value -= event.clientX - lastDragX;
-    offsetY.value -= event.clientY - lastDragY;
-    lastDragX = event.clientX;
-    lastDragY = event.clientY;
-    console.log(`drag occurred @ ${lastDragX}, ${lastDragY}`);
+  mouseScreenX.value = event.clientX;
+  mouseScreenY.value = event.clientY;
+
+  if (isDragging.value) {
+    offsetX.value -= event.movementX;
+    offsetY.value -= event.movementY;
   }
 };
 
@@ -57,13 +120,31 @@ onUnmounted(() => {
 </script>
 
 <template>
-  <svg :width="windowWidth.value" :height="windowHeight.value" :viewBox="viewBox">
-    <circle cx="200" cy="200" r="50" fill="red"></circle>
-    <rect x="400" y="300" width="100" height="50" fill="green" />
+  <svg class="cursor" :style="cursorStyle" viewBox="0 0 32 32" width="32" height="32">
+    <path d="M0,0 L32,8 L16,16 L8,32 Z" fill="black"></path>
   </svg>
+
+  <svg :width="windowWidth.value" :height="windowHeight.value" :viewBox="viewBox" class="board">
+    <template v-for="shape in shapes">
+      <circle v-if="shape.kind == 'circle'" :key="shape.id" :cx="shape.center.x" :cy="shape.center.y" :r="shape.radius"
+              fill="red" />
+      <rect v-if="shape.kind == 'rect'" :key="shape.id" :x="shape.boundingBox.x" :y="shape.boundingBox.y"
+            :width="shape.boundingBox.width" :height="shape.boundingBox.height" fill="red" />
+    </template>
+  </svg>
+
+  <div class="debug-info">
+    <div>Offset: {{ offsetX }}, {{ offsetY }}</div>
+    <div>Size: {{ windowWidth }}, {{ windowHeight }}</div>
+    <div>Shapes: {{ shapes.length }}</div>
+  </div>
 </template>
 
 <style scoped>
+* {
+  cursor: none !important;
+}
+
 .board {
   position: absolute;
   top: 0;
@@ -72,5 +153,24 @@ onUnmounted(() => {
   bottom: 0;
   margin: 0;
   padding: 0;
+}
+
+.cursor {
+  position: absolute;
+  z-index: 101;
+}
+
+.debug-info {
+  position: absolute;
+  z-index: 100;
+  top: 1em;
+  left: 1em;
+  margin: 0;
+  padding: 10px;
+
+  color: red;
+  border: 1px solid black;
+  background-color: white;
+  opacity: 0.6;
 }
 </style>
