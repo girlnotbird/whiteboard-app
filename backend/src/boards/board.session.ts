391b77bf908093fabf6d528bc1f910f7ed58968a
Jessie McCollum <mccoljessie@gmail.com>
2024-11-19 00:21:49 -0500

    some clean up of BoardSession, BoardView
    - incorporating notes from review of PR #15
    
    - add flag to backend for toggling whether WebSocket clients receive their own messages
    
    - add an abort signal to BoardSession event listeners, fix binding of event handlers
    
    - cleaned up BoardSession.vue using @vueuse/rxjs, mapped elements to their IDs, moved
    schema validation to the event publisher and removed from event receiver
    
    - changed eventStream event type from IRawBoardEvent to IBoardEvent

--- backend/src/boards/board.session.ts
+++ backend/src/boards/board.session.ts
@@ -2,6 +2,7 @@ import { WebSocket } from 'ws';
 import { Logger } from '@nestjs/common';
 
 export class BoardSession {
+  private ENABLE_TX_TO_SELF: boolean = true;
   private readonly members = new Map<number, WebSocket>();
   private nextMemberId = 0;
 
@@ -27,14 +28,18 @@ export class BoardSession {
   private startDrivingMember(id: number, socket: WebSocket) {
     this.members.set(id, socket);
 
-    socket.addEventListener('message', (event) => {
-      for (const [otherId, otherSocket] of this.members.entries()) {
-        if (otherId == id) {
-        } //continue;
+    socket.addEventListener(
+      'message',
+      ((event) => {
+        for (const [otherId, otherSocket] of this.members.entries()) {
+          if (!this.ENABLE_TX_TO_SELF && otherId == id) {
+            continue;
+          }
 
-        otherSocket.send(event.data);
-      }
-    });
+          otherSocket.send(event.data);
+        }
+      }).bind(this),
+    );
 
     socket.addEventListener('close', () => {
       this.members.delete(id);
